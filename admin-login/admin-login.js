07a04cfb81697c5f87c616d7556956157ab30232
YuriMeneghin <yuri.meneghin@edu.itspiemonte.it>
2024-09-25 14:30:38 +0200

    update admin login.js
    so scemo

--- admin-login/admin-login.js
+++ admin-login/admin-login.js
@@ -9,50 +9,50 @@ document.getElementById('languageSelector').addEventListener('change', function(
           email.placeholder = 'Email';
           password.placeholder = 'Password';
           loginButton.textContent = 'Login';
-          forgotPassword.textContent = 'Hai dimenticato la password?';
+          forgotPassword.textContent = 'Hai bisogno di assistenza?';
         } else if (language === 'en') {
           email.placeholder = 'Email';
           password.placeholder = 'Password';
           loginButton.textContent = 'Login';
-          forgotPassword.textContent = 'Forgot your password?';
+          forgotPassword.textContent = 'Do you need assistance?';
         } else if (language === 'fr') {
           email.placeholder = 'Email';
           password.placeholder = 'Password';
           loginButton.textContent = 'Login';
-          forgotPassword.textContent = 'Mot de passe oublié?';
+          forgotPassword.textContent = 'Avez-vous besoin d\'aide?';
         }
         else if(language === 'es') {
             email.placeholder = 'Email';
             password.placeholder = 'Password';
             loginButton.textContent = 'Login';
-            forgotPassword.textContent = '¿Olvidaste tu contraseña?';
+            forgotPassword.textContent = '¿Necesitas ayuda?';
         }
         else if(language === 'ru')
         {
             email.placeholder = 'электронная почта';
             password.placeholder = 'пароль';
             loginButton.textContent = 'авторизоваться';
-            forgotPassword.textContent = 'Забыли пароль?';
+            forgotPassword.textContent = 'Вам нужна помощь?';
         }
         else if(language === 'ch')
         {
             email.placeholder = '电子邮件';
             password.placeholder = '密码';
             loginButton.textContent = '登录';
-            forgotPassword.textContent = '你好，密码是多少？';
+            forgotPassword.textContent = '你需要帮助吗？';
         }
         else if(language === 'dt')
         {
             email.placeholder = 'Email';
             password.placeholder = 'Passwort';
             loginButton.textContent = 'Login';
-            forgotPassword.textContent = 'Hast du das Passwort vermisst?';
+            forgotPassword.textContent = 'Benötigen Sie Hilfe?';
         }
         else if(language === 'tk')
         {
           email.placeholder = 'e-posta';
           password.placeholder = 'şifre';
           loginButton.textContent = 'Giriş';
-          forgotPassword.textContent = 'Şifreniz mi unuttunuz?';
+          forgotPassword.textContent = 'Yardıma ihtiyacınız var mı?';
         }
       });
