98d2ee398cefebea4fb826bed27bd0bea2955465
doodshot <guido.panzieri@edu.itspiemonte.it>
2024-09-30 20:05:11 +0200

    aggiunto lingue su placeholder su login user(finito anche quella schermata)

--- user login/script.js
+++ user login/script.js
@@ -2,13 +2,16 @@ document.getElementById('languageSelector').addEventListener('change', function(
     const language = this.value;
     const textElement = document.getElementById('Qr-user');
     const adminLink = document.getElementById('submit-button');
+    const inputElement = document.querySelector('.input-field');
   
     if (language === 'it') {
       textElement.textContent = 'Vuoi scansionare nuovamente il QR?';
       adminLink.textContent = 'Invia';
+      inputElement.placeholder = 'Inserisci l\'anno di acquisto';
     } else if (language === 'en') {
       textElement.textContent = 'You need to scan the QR code again';
       adminLink.textContent = 'submit';
+      inputElement.placeholder = 'Enter the purchase year';
     } else if (language === 'fr') {
       textElement.textContent = 'Vous devez scanner à nouveau le code QR';
       adminLink.textContent = 'Envoyer';
@@ -16,26 +19,31 @@ document.getElementById('languageSelector').addEventListener('change', function(
     else if(language === 'es') {
         textElement.textContent = 'Debes escanear el código QR de nuevo';
         adminLink.textContent = 'Enviar';
+        inputElement.placeholder = 'Introduce el año de compra';
     }
     else if(language === 'ru') 
     {
         textElement.textContent = 'Сначала вам нужно сканировать QR код';
         adminLink.textContent = 'Отправить';
+        inputElement.placeholder = 'Введите год покупки';
     }
     else if(language === 'ch')
     {
         textElement.textContent = '您需要再次扫描二维码';
         adminLink.textContent = '发送';
+        inputElement.placeholder = '输入购买年份';
     }
     else if(language === 'dt')
     {
         textElement.textContent = 'Sie müssen den QR-Code erneut scannen';
         adminLink.textContent = 'Senden';
+        inputElement.placeholder = 'Einkaufsjahr eingeben';
     }
     else if(language === 'tk')
     {
       textElement.textContent = 'QR kodunu yeniden taramanız gerekiyor';
       adminLink.textContent = 'Gönder';
+      inputElement.placeholder = 'Satın alma yılını girin';
     }
   });
 
