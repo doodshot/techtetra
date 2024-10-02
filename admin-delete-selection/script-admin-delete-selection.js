451596079d11d3f230555e3f6b641b98d5e23c66
ICTS2325071 <mattia.cellerino@edu.itspiemonte.it>
2024-10-02 12:48:28 +0200

    update

--- admin-delete-selection/script-admin-delete-selection.js
+++ admin-delete-selection/script-admin-delete-selection.js
@@ -1,40 +1,31 @@
 document.getElementById('languageSelector').addEventListener('change', function() {
     const language = this.value;
-    const textElement = document.querySelector('.testo');
-    const adminLink = document.getElementById('admin-login');
+    const textElement = document.querySelector('.file-eliminato');
   
     if (language === 'it') {
-      textElement.textContent = 'Devi prima scansionare il QR Code';
-      adminLink.textContent = 'Sei un amministratore?';
+      textElement.textContent = 'File eliminato correttamente!';
     } else if (language === 'en') {
-      textElement.textContent = 'You must scan the QR Code first';
-      adminLink.textContent = 'Are you an administrator?';
+      textElement.textContent = 'File deleted successfully!';
     } else if (language === 'fr') {
-      textElement.textContent = 'Vous devez d\'abord scanner le QR Code';
-      adminLink.textContent = 'Êtes-vous administrateur?';
+      textElement.textContent = 'Fichier supprimé avec succès!';
     }
     else if(language === 'es') {
-        textElement.textContent = 'Primero debes escanear el código QR';
-        adminLink.textContent = '¿Eres administrador?';
+        textElement.textContent = '¡Archivo eliminado exitosamente!';
     }
     else if(language === 'ru') 
     {
-        textElement.textContent = 'Сначала вам нужно сканировать QR код';
-        adminLink.textContent = 'Вы - менеджер?';
+        textElement.textContent = 'Fișierul a fost șters cu succes!';
     }
     else if(language === 'ch')
     {
-        textElement.textContent = '您需要先扫描二维码';
-        adminLink.textContent = '您是管理员吗？';
+        textElement.textContent = '文件刪除成功!';
     }
     else if(language === 'dt')
     {
-        textElement.textContent = 'Sie müssen zuerst den QR-Code scannen';
-        adminLink.textContent = 'Sind Sie ein Administrator?';
+        textElement.textContent = 'Datei erfolgreich gelöscht!';
     }
     else if(language === 'tk')
     {
-      textElement.textContent = 'Önce QR kodunu taratmanız gerekiyor';
-      adminLink.textContent = 'Yönetici misiniz?';
+      textElement.textContent = 'Dosya başarıyla silindi!';
     }
 });
